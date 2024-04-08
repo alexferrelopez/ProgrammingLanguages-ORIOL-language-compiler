e85604de70adaf2611605665f705d062a16414e7
Àlex Ferré López <alex.fl@students.salle.url.edu>
2024-04-08 20:20:16 +0200

    TokenType.java enumeration added for every existing token. Token class to store information for each token. Scanner in progress

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,2 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class LexicalAnalyzer {
+    /**
+     * Lexical Analyzer / Scanner
+     **/
+    private BufferedReader bufferedReader;
+    private List<Token> tokens;
+    private int currentLine;
+    private int index;
+    private String line;
+
+    public LexicalAnalyzer(String inputFileName) {
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFileName));
+            line = bufferedReader.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Token nextToken() {
+        //return token;
+        return tokens.get(index++);
+    }
 }
