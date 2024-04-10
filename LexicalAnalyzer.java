86bc8ae8bb91b0d8db07b8ed9158f93991770b9c
Àlex Ferré López <alex.fl@students.salle.url.edu>
2024-04-10 13:03:38 +0200

    Un test més per comprovar que es llegeix tot el fitxer. Petites millores.

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,23 +1,25 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+
 
 public class LexicalAnalyzer {
     /**
      * Lexical Analyzer / Scanner
      **/
     private BufferedReader bufferedReader;
-    private List<Token> tokens;
-    private int currentLine;
-    private int index;
+    //private List<Token> tokens;
+    //private int currentLine;
+    //private int index;
     private String line;
     private int lineIndex;
+    private List<String> words;
 
     public LexicalAnalyzer(String inputFileName) {
         try {
             bufferedReader = new BufferedReader(new FileReader(inputFileName));
-            line = bufferedReader.readLine();
+            nextLine();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,29 +30,39 @@ public class LexicalAnalyzer {
         try {
             line = bufferedReader.readLine();
             lineIndex = 0;
+            words = Collections.emptyList();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public String getNextWord() {
+
+        // EoF reached
         if (line == null) {
             return null;
         }
-        String[] words = line.split(" ");
-        if (words.length == 0) {
+
+        if (words.isEmpty()) {
+            words = Arrays.stream(line.split("(?=[ ;)({}:,])")).toList();
+        }
+
+        if (words.isEmpty()) {
             nextLine();
             return getNextWord();
         }
-        String word = words[lineIndex++];
-        if (lineIndex == words.length) {
+
+        String word = words.get(lineIndex++);
+        if (lineIndex >= words.size()) {
             nextLine();
         }
+        //return word.trim();
         return word;
     }
-
+    /*
     public Token nextToken() {
         //return token;
-        return tokens.get(index++);
-    }
+        return null;
+    }*/
 }
--- LexicalAnalyzerTest.java
+++ LexicalAnalyzerTest.java
@@ -7,10 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LexicalAnalyzerTest {
 
+    // Checks if words are separated by tokens
     @Test
     void wordByWordRead() {
         ArrayList<String> testWords = getWords();
-        ArrayList<String> expectedWords = new ArrayList<>(List.of("ASD QE SX SDF S SG".split(" ")));
+        String s = """
+                oink: sumRange(oink numA, oink numB, oink increment) {
+                    oink result is 0.0;
+                               \s
+                    check (numA bigger numB) {
+                        oink tmp is numA;
+                        numA is numB;
+                        numB is tmp;
+                    }
+                               \s
+                    feed (oink i is numA to numB, i is i sum increment) {
+                        result is result sum i;
+                    }
+                               \s
+                    poop result;
+                }
+                               \s
+                miau: ranch() {
+                    oink a is 3.23;
+                    oink b is 4.5;
+                    oink c is 0.2;
+                    oink total is sumRange(a, b, c);
+                    poop 0;
+                }
+               \s""";
+        ArrayList<String> expectedWords = new ArrayList<>(List.of(s));
         for (int i = 0; i < testWords.size(); i++) {
             String word = testWords.get(i);
             String expectedWord = expectedWords.get(i);
@@ -18,9 +44,48 @@ class LexicalAnalyzerTest {
         }
     }
 
+    // This checks all content is split into tokens and no content is lost from the file, \n are removed since we read line by line
+    @Test
+    void allContentIsTokenized() {
+        // join all words into a single string
+        ArrayList<String> testWords = getWords();
+        StringBuilder sb = new StringBuilder();
+        for (String word : testWords) {
+            sb.append(word);
+        }
+        String testContent = sb.toString();
+        String s = """
+                oink: sumRange(oink numA, oink numB, oink increment) {
+                    oink result is 0.0;
+                               \s
+                    check (numA bigger numB) {
+                        oink tmp is numA;
+                        numA is numB;
+                        numB is tmp;
+                    }
+                               \s
+                    feed (oink i is numA to numB, i is i sum increment) {
+                        result is result sum i;
+                    }
+                               \s
+                    poop result;
+                }
+                               \s
+                miau: ranch() {
+                    oink a is 3.23;
+                    oink b is 4.5;
+                    oink c is 0.2;
+                    oink total is sumRange(a, b, c);
+                    poop 0;
+                }
+               \s""".replace("\n", "");
+        assertEquals(testContent, s);
+
+    }
+
     private ArrayList<String> getWords() {
         ArrayList<String> words = new ArrayList<>();
-        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer("ExempleParaules.farm");
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer("Exemple1.farm");
         String nextWord;
         do {
             nextWord = lexicalAnalyzer.getNextWord();
