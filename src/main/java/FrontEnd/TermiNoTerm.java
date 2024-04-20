a19b2acfd8e8d7d554667853ca2cce62ab396489
Alexia <alexia.cabrera@students.salle.url.edu>
2024-04-20 11:30:17 +0200

    terminal i no terminal

---
+++ src/main/java/FrontEnd/TermiNoTerm.java
@@ -0,0 +1,13 @@
+package FrontEnd;
+
+public abstract class TermiNoTerm {
+    private String name;
+
+    public TermiNoTerm(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
