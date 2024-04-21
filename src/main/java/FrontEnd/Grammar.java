6a1e99ef41fb3283d1a466d864aa588690a5c58b
Oriol González Gómez <oriol.gg@students.salle.url.edu>
2024-04-21 10:56:33 +0200

    commented code for better understanding :)

--- src/main/java/FrontEnd/Grammar.java
+++ src/main/java/FrontEnd/Grammar.java
@@ -6,6 +6,13 @@ import java.util.*;
 
 public class Grammar {
     private final String GRAMMAR_PATH = "src/test/resources/gramatica.txt";
+    /**
+     * Our structure to store the whole grammar.
+     * It is a HashMap where its Key will be each non-terminal and the Value will be the different productions from that non-terminal.
+     * To store the different productions we are using a double LinkedList.
+     *      * The first one will store the different productions each non-terminal can lead to.
+     *      * And the second list will be each of the Terminals and Non-Terminals that each particular production can result in.
+     */
     private Map<NoTerminal, List<List<TermiNoTerm>>> grammar = new HashMap<>();
     private List<NoTerminal> noTerminalList = new LinkedList<>();
 
@@ -17,6 +24,12 @@ public class Grammar {
         this.readGrammar();
     }
 
+    /**
+     * Casts a String like <name> to a NoTerminal Type
+     * @param text String to convert to NoTerminal
+     * @param isFirst if it's the first means is the axioma
+     * @return the NoTerminal
+     */
     private NoTerminal getNoTerminal(String text, boolean isFirst){
         text = text.trim();
         text = text.replaceAll("<", "");
@@ -24,6 +37,12 @@ public class Grammar {
         return findNoTerminal(text, isFirst);
     }
 
+    /**
+     * Returns a NoTerminal given a string
+     * @param noTerm NoTerminal to return
+     * @param isFirst if it is the axioma
+     * @return the NoTerminal
+     */
     private NoTerminal findNoTerminal(String noTerm, boolean isFirst){
         for(NoTerminal nt: noTerminalList){
             if(nt.getName().equals(noTerm)){
@@ -35,6 +54,11 @@ public class Grammar {
         return nt;
     }
 
+    /**
+     * get the production of each NoTerminal from our gramamr
+     * @param n NoTerminal to find it's producion
+     * @param text Each line of our grammar file
+     */
     private void getDreta(NoTerminal n, String text){
         text = text.trim();
         int punter = 0;
@@ -44,11 +68,11 @@ public class Grammar {
         StringBuilder sbNOterm = new StringBuilder();
         StringBuilder sbTerm = new StringBuilder();
         grammar.get(n).add(new LinkedList<>());
-        for (char c:charArray) {
+        for (char c:charArray) { //Loop each character of the line
             if(esNoTerm){
-                if(c != '>'){
+                if(c != '>'){ //If its no terminal and the current character is not '>' means the character is part of the no terminal
                     sbNOterm.append(c);
-                }else{
+                }else{ //Once we found a '>' means the NoTerminal has ended, and we have to store it in our grammar
                     String noTerm = sbNOterm.toString();
                     NoTerminal nt = findNoTerminal(noTerm, false);
                     grammar.get(n).get(punter).add(nt);
@@ -56,24 +80,24 @@ public class Grammar {
                     esNoTerm = false;
                 }
             }else{
-                if(c == '<'){
+                if(c == '<'){ //If we found a '<' means a NoTerminal will start
                     esNoTerm = true;
-                    if(esTerm){
+                    if(esTerm){//If we were analyzing a Terminal and we found a '<' means a NoTerminal starts hence the Terminal finishes and we have to store it in our grammar
                         String str = sbTerm.toString();
                         Terminal t = new Terminal(str);
                         grammar.get(n).get(punter).add(t);
                         sbTerm = new StringBuilder();
                         esTerm = false;
                     }
-                }else if(c == '>'){
+                }else if(c == '>'){//If we found a '>' it won't be a NoTerminal no more
                     esNoTerm = false;
-                }else if(c == '|'){
+                }else if(c == '|'){ //If we found a '>' means we have another production for the same non-terminal
                     grammar.get(n).add(new LinkedList<>());
                     punter++;
-                }else if(c == 'ε'){
+                }else if(c == 'ε'){//Is a Epsilon
                     Terminal epsilon = new Terminal(String.valueOf(c));
                     grammar.get(n).get(punter).add(epsilon);
-                }else if(c == ' '){
+                }else if(c == ' '){ //If we found a space and we were analyzing a Terminal it means that the Terminal has ended hence we have to store it
                     if(esTerm){
                         String str = sbTerm.toString();
                         Terminal t = new Terminal(str);
@@ -87,7 +111,7 @@ public class Grammar {
                 }
             }
         }
-        if(esTerm){
+        if(esTerm){//Once we ended looping through each character, we need to know if the last char was part of a Terminal to store it to our grammar
             String str = sbTerm.toString();
             Terminal t = new Terminal(str);
             grammar.get(n).get(punter).add(t);
@@ -103,7 +127,7 @@ public class Grammar {
             boolean isFirst = true;
             while (sc.hasNextLine()) {
                 String data = sc.nextLine();
-                String[] hashMapSeparator = data.split("::=");
+                String[] hashMapSeparator = data.split("::=");//Since our grammar is in BNF nomenclature, we can split each line through the '::=' string and get the non-terminal on the first position and the productions on the second production
                 List<List<TermiNoTerm>> termINoTermList = new LinkedList<>();
                 NoTerminal nt = getNoTerminal(hashMapSeparator[0], isFirst);
                 isFirst = false;
