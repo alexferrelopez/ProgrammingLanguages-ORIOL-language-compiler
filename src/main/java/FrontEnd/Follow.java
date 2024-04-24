98712e22759ab9b7f3ad1868ac0fb1d9620b846f
Oriol González Gómez <oriol.gg@students.salle.url.edu>
2024-04-24 13:19:28 +0200

    Obtain a terminal list of all the follows of a given non-terminal

--- src/main/java/FrontEnd/Follow.java
+++ src/main/java/FrontEnd/Follow.java
@@ -12,12 +12,64 @@ public class Follow {
      * @param terminal terminal to be added to the list
      */
     private static void addFollow(List<Terminal> follows, Terminal terminal){
-        if(terminal.getName().equals("Ɛ")) return;
+        if(terminal.getName().equals(Terminal.EPSILON)) return;
         for(Terminal follow: follows){
             if(follow.getName().equals(terminal.getName())){
                 return;
             }
         }
         follows.add(terminal);
+    }
+
+    /**
+     * Method for obtaining a terminal list of all the follows of a given non-terminal
+     * @param grammar Our grammar
+     * @param noTerminal The non-terminal to be found its follows
+     * @return list of all the follows of the given non-terminal
+     */
+    public static List<Terminal> getFollows(Map<NoTerminal, List<List<TermiNoTerm>>> grammar, NoTerminal noTerminal) {
+        List<Terminal> follows = new LinkedList<>();
+        if(noTerminal.isAxioma()){
+            addFollow(follows, new Terminal("EOF"));
+        }
+        for(Map.Entry<NoTerminal, List<List<TermiNoTerm>>> entry : grammar.entrySet()){
+            for(List<TermiNoTerm> symbol  :entry.getValue()){
+                for(TermiNoTerm tint: symbol){
+                    if(tint.equals(noTerminal)){
+                        int pos = symbol.indexOf(noTerminal);
+                        if(symbol.size()>pos+1){
+                            TermiNoTerm next = symbol.get(pos+1);
+                            if(next.isTerminal()){
+                                addFollow(follows, (Terminal) next);
+                            }else{
+                                List<Terminal> firsts = First.getFirsts(grammar, (NoTerminal) next);
+                                for (Terminal t:firsts) {
+                                    if(!t.getName().equals(Terminal.EPSILON)){
+                                        addFollow(follows, t);
+                                    }else{
+                                        NoTerminal nt = entry.getKey();
+                                        if(!nt.equals(noTerminal)){
+                                            List<Terminal> terminals = getFollows(grammar, nt);
+                                            for(Terminal followTerminals: terminals){
+                                                addFollow(follows, followTerminals);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }else{
+                            NoTerminal nt = entry.getKey();
+                            if(!nt.equals(noTerminal)){
+                                List<Terminal> terminals = getFollows(grammar, nt);
+                                for(Terminal followTerminals: terminals){
+                                    addFollow(follows, followTerminals);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return follows;
     }
 }
