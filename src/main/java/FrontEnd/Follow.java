6ad30f49503234a25fa6396e34e9d9499e6865a4
Alexia <alexia.cabrera@students.salle.url.edu>
2024-04-24 13:09:06 +0200

    Adding follows to the list

--- src/main/java/FrontEnd/Follow.java
+++ src/main/java/FrontEnd/Follow.java
@@ -1,4 +1,23 @@
 package FrontEnd;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Follow {
+    /**
+     * Method for adding follows
+     * @param follows follows list
+     * @param terminal terminal to be added to the list
+     */
+    private static void addFollow(List<Terminal> follows, Terminal terminal){
+        if(terminal.getName().equals("Ɛ")) return;
+        for(Terminal follow: follows){
+            if(follow.getName().equals(terminal.getName())){
+                return;
+            }
+        }
+        follows.add(terminal);
+    }
 }
