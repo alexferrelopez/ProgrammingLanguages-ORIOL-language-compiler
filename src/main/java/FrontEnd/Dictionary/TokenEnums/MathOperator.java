22de52ee0828979fcdf8f7328a2049a15f525fca
Àlex Ferré López <alex.fl@students.salle.url.edu>
2024-04-25 16:51:02 +0200

    translations added for all tokens, some left empty to ignore them later

--- src/main/java/FrontEnd/Dictionary/TokenEnums/MathOperator.java
+++ src/main/java/FrontEnd/Dictionary/TokenEnums/MathOperator.java
@@ -2,26 +2,35 @@ package FrontEnd.Dictionary.TokenEnums;
 
 import FrontEnd.Dictionary.TokenType;
 
+import java.util.List;
+
 public enum MathOperator implements TokenType {
 	// Enum constants (mathematics operators)
-	SUM("sum"),
-	SUB("minus"),
-	MOD("mod|module"),
-	MUL("times"),
-	POW("pow|power"),
-	DIV("div");
+	SUM("sum", List.of("sum")),
+	SUB("minus", List.of("minus")),
+	MOD("mod|module", List.of("mod", "module")),
+	MUL("times", List.of("times")),
+	POW("pow|power", List.of("pow", "power")),
+	DIV("div", List.of("div"));
 
 	// Instance field (regex pattern) for each enum constant
 	private final String regexPattern;
+	private final List<String> translation;
 
 	// Constructor to initialize the instance field (allow it to have a string value)
-	MathOperator(String pattern) {
+	MathOperator(String pattern, List<String> translation) {
 		this.regexPattern = pattern;
+		this.translation = translation;
 	}
 
 	// Getter method for the pattern
 	@Override
 	public String getPattern() {
 		return this.regexPattern;
+	}
+
+	@Override
+	public List<String> getTranslation() {
+		return this.translation;
 	}
 }
--- src/main/java/FrontEnd/Dictionary/TokenEnums/ReservedSymbol.java
+++ src/main/java/FrontEnd/Dictionary/TokenEnums/ReservedSymbol.java
@@ -2,35 +2,44 @@ package FrontEnd.Dictionary.TokenEnums;
 
 import FrontEnd.Dictionary.TokenType;
 
+import java.util.List;
+
 public enum ReservedSymbol implements TokenType {
 	// Enum constants (special symbols)
-	IF("check"),
-	ELSE("else|otherwise"),
-	WHILE("breed"),
-	FOR("feed"),
-	DO("do"),
-	TO("to"),
-	RETURN("poop"),
-	EOF("EOF"),
-	MAIN("ranch"),
-	COMMENT("Farmer: " + ValueSymbol.VALUE_STRING.getPattern()),	// Farmer: VALOR_STRING
-	AARON("(?i)aaron"),											// Regex pattern case insensitive by adding (?i)
-	ORIOL("(?i)oriol"),
-	ALEXIA("(?i)al(e|è)xia"),
-	GEMMA("(?i)gemma"),
-	ALEX("(?i)(a|à)lex");
+	IF("check", List.of("check")),
+	ELSE("else|otherwise", List.of("else", "otherwise")),
+	WHILE("breed", List.of("breed")),
+	FOR("feed", List.of("feed")),
+	DO("do", List.of("do")),
+	TO("to", List.of("to")),
+	RETURN("poop", List.of("poop")),
+	EOF("EOF", List.of("EOF")),
+	MAIN("ranch", List.of("ranch")),
+	COMMENT("Farmer: " + ValueSymbol.VALUE_STRING.getPattern(), List.of("")),	// Farmer: VALOR_STRING
+	AARON("(?i)aaron", List.of("aaron")),																		// Regex pattern case insensitive by adding (?i)
+	ORIOL("(?i)oriol", List.of("oriol")),
+	ALEXIA("(?i)al(e|è)xia", List.of("alexia", "alèxia")),
+	GEMMA("(?i)gemma", List.of("gemma")),
+	ALEX("(?i)(a|à)lex", List.of("alex", "àlex")),;
 
 	// Instance field (regex pattern) for each enum constant
 	private final String regexPattern;
+	private final List<String> translation;
 
 	// Constructor to initialize the instance field (allow it to have a string value)
-	ReservedSymbol(String pattern) {
+	ReservedSymbol(String pattern, List<String> translation) {
 		this.regexPattern = pattern;
+		this.translation = translation;
 	}
 
 	// Getter method for the pattern
 	@Override
 	public String getPattern() {
 		return this.regexPattern;
+	}
+
+	@Override
+	public List<String> getTranslation() {
+		return this.translation;
 	}
 }
