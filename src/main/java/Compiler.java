51ea8f7648fca9f4a536fd79798b134b735eeffd
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2024-04-30 14:44:10 +0200

    · Comentaris per la interfície del compilador.

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -15,11 +15,18 @@ public class Compiler implements CompilerInterface {
 		// ---- BACK END ---- //
 	}
 
+	/**
+	 * This method starts the lexical, syntactic and semantic analysis of the code. Generates the intermediate code.
+	 */
 	@Override
 	public void compileCode() {
 		parser.parseProgram();
 	}
 
+	/**
+	 * This method returns if the code has errors or not (checks the lexical, syntactic and semantic errors).
+	 * @return True if the code has errors, false otherwise.
+	 */
 	@Override
 	public boolean hasErrors() {
 		return false;
