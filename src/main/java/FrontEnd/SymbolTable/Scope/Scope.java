a9373d82d1e06a87ac8c17b7e11fe9ce6b0ba5a4
Gemma Yebra Berenguer <gemma.yebra@salle.url.edu>
2024-04-30 18:08:49 +0200

    Fer que el scope sigui un node.

--- src/main/java/FrontEnd/SymbolTable/Scope/Scope.java
+++ src/main/java/FrontEnd/SymbolTable/Scope/Scope.java
@@ -2,29 +2,18 @@ package FrontEnd.SymbolTable.Scope;
 
 import FrontEnd.SymbolTable.Symbol.Symbol;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scope {
-	private Scope nextScope;
-	private int scopeId; // Scope ID, represents the level of the scope
-
-	private final Map<String, Symbol<?>> scopeSymbols = new HashMap<>();
+	private List<Scope> children = new ArrayList<>();
+	private Scope parent;
+	private int scopeId; 					// Scope ID, represents the level of the scope
+	private List<Symbol> symbols; 		// List of symbols in the scopes
 
 	public Scope(int scopeId) {
 		this.scopeId = scopeId;
-	}
-
-	/**
-	 * Check if the symbol name is unique in the scope.
-	 * @param name	the name of the symbol.
-	 */
-	public void checkUniqueSymbolName(String name) {
-		for (String local_name : this.scopeSymbols.keySet()) {
-			if (local_name.equals(name)) {
-				// TODO -> Error: Symbol already defined
-			}
-		}
 	}
 
 	/**
@@ -32,8 +21,7 @@ public class Scope {
 	 * @param symbol	the symbol to add.
 	 */
 	public void addSymbol(Symbol<?> symbol) {
-		this.checkUniqueSymbolName(symbol.getName());
-		this.scopeSymbols.put(symbol.getName(), symbol);
+		this.symbols.add(symbol);
 	}
 
 	/**
@@ -42,15 +30,30 @@ public class Scope {
 	 * @return	the symbol with the given name, or null if the symbol is not in the scope.
 	 */
 	public Symbol<?> findSymbol(String name) {
-		for (Map.Entry<String, Symbol<?>> symbol_entry : this.scopeSymbols.entrySet()) {
-			String symbolName = symbol_entry.getKey();
-			Symbol<?> symbolValue = symbol_entry.getValue();
-
-			if (name.equals(symbolName)) {
-				return symbolValue;
+		for (Symbol<?> symbol : this.symbols) {
+			if (symbol.getName().equals(name)) {
+				return symbol;
 			}
 		}
 		return null;
 	}
 
+	/**
+	 * Get the ID of the scope.
+	 * @return	the ID of the scope.
+	 */
+	public int getID() {
+		return this.scopeId;
+	}
+
+	public void addChild(Scope scope) {
+	}
+
+	public List<Scope> getChildren() {
+		return children;
+	}
+
+	public void removeChild(Scope scope) {
+		this.children.remove(scope);
+	}
 }
