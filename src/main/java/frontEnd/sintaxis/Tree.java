43c364b72904ed72e510590a404546f4ab6fe105
Oriol González Gómez <oriol.gg@students.salle.url.edu>
2024-05-05 22:05:01 +0200

    Update the tree

--- src/main/java/frontEnd/sintaxis/Tree.java
+++ src/main/java/frontEnd/sintaxis/Tree.java
@@ -7,13 +7,22 @@ public class Tree<T> {
     private final LinkedList<Tree<T>> children;
     private T parent;
 
-    public Tree(T node) {
+    public Tree(T node){
         this.node = node;
         children = new LinkedList<>();
         parent = null;
     }
 
-    public Tree<T> addChild(T child) {
+    public Tree(Tree t){
+        this.node = (T) t.node;
+        children = new LinkedList<>();
+        parent = (T) t.parent;
+        for (var c : t.children){
+            children.add(new Tree(c));
+        }
+    }
+
+    public Tree<T> addChild(T child){
         var c = new Tree<T>(child);
         children.add(c);
         c.parent = (T) this;
@@ -28,5 +37,7 @@ public class Tree<T> {
         return children;
     }
 
-
+    public T getParent() {
+        return parent;
+    }
 }
