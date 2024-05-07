b7768c0f20e854cb2ee33fcc50071fe26d8f2f61
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2024-05-08 00:14:31 +0200

    · Constants d'errors de semàntica afegits.

--- src/main/java/errorHandlers/errorTypes/SemanticErrorType.java
+++ src/main/java/errorHandlers/errorTypes/SemanticErrorType.java
@@ -4,6 +4,14 @@ package errorHandlers.errorTypes;
  * Enum for semantic errors
  */
 public enum SemanticErrorType implements ErrorType {
+    VARIABLE_NOT_DECLARED("Variable not declared"),
+    DUPLICATE_SYMBOL_DECLARATION("Duplicate symbol declaration"),
+    INCOMPATIBLE_TYPES("Incompatible types"),
+    FUNCTION_NOT_DECLARED("Function not declared"),
+    FUNCTION_NOT_CALLED_CORRECTLY("Function not called correctly"),
+    FUNCTION_PARAMETERS_NOT_MATCH("Function parameters not match"),
+    FUNCTION_RETURN_TYPE_NOT_CORRECT("Function return type not correct"),
+    FUNCTION_RETURN_NOT_CORRECT("Function return not correct"),
     ;
 
     //TODO: Add different types of errors,
