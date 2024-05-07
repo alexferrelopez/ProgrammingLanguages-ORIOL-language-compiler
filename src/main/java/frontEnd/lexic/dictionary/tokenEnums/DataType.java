3ec83f92a826c99f5e95aae46bdceaa5a5d19a05
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2024-05-07 23:12:52 +0200

    · S'han recuperat les traduccions del DataType.

--- src/main/java/frontEnd/lexic/dictionary/tokenEnums/DataType.java
+++ src/main/java/frontEnd/lexic/dictionary/tokenEnums/DataType.java
@@ -6,22 +6,24 @@ import java.util.List;
 
 public enum DataType implements TokenType {
     // Enum constants (data types)
-    INTEGER("miau", 4, Integer.MAX_VALUE, Integer.MIN_VALUE),	// Each integer is 4 Bytes.
-    FLOAT("oink", 4, Float.MAX_VALUE, -Float.MAX_VALUE),		// Each float is 4 Bytes.
-    BOOLEAN("status", 1, 1, 0),				// 1 = true, 0 = false. Each boolean is 1 Byte.
-    CHAR("moo", 1, (int) Character.MAX_VALUE, (int) Character.MIN_VALUE),	// Each character is 1 Byte.
-    STRING("quack", 1, Integer.MAX_VALUE, 0),			// Each character is 1 Byte (multiply it by the String's length).
-    VOID("void", 0, 0, 0);		// Each void is 0 Bytes.
+    INTEGER("miau", List.of("miau"), 4, Integer.MAX_VALUE, Integer.MIN_VALUE),	            // Each integer is 4 Bytes.
+    FLOAT("oink", List.of("oink"), 4, Float.MAX_VALUE, -Float.MAX_VALUE),		            // Each float is 4 Bytes.
+    BOOLEAN("status", List.of("status"), 1, 1, 0),				        // 1 = true, 0 = false. Each boolean is 1 Byte.
+    CHAR("moo", List.of("moo"), 1, (int) Character.MAX_VALUE, (int) Character.MIN_VALUE),  // Each character is 1 Byte.
+    STRING("quack", List.of("quack"), 1, Integer.MAX_VALUE, 0),			        // Each character is 1 Byte (multiply it by the String's length).
+    VOID("void", List.of("void"), 0, 0, 0);		                    // Each void is 0 Bytes.
 
     // Instance field (regex pattern) for each enum constant
     private final String regexPattern;
+    private final List<String> translation;
     private final int size; // Size of the data type in Bytes
     private final Number maxValue; // Maximum value that this data type can hold
     private final Number minValue; // Minimum value that this data type can hold
 
     // Constructor to initialize the instance fields (allow it to have a string value and its size in Bytes).
-    DataType(String pattern, int size, Number maxValue, Number minValue) {
+    DataType(String pattern, List<String> translation, int size, Number maxValue, Number minValue) {
         this.regexPattern = pattern;
+        this.translation = translation;
         this.size = size;
         this.maxValue = maxValue;
         this.minValue = minValue;
@@ -35,7 +37,7 @@ public enum DataType implements TokenType {
 
     @Override
     public List<String> getTranslation() {
-        return List.of();
+        return this.translation;
     }
 
     // Getter method for the size of the data type
