4ffd00ea7cf6868390263d6ae10d9ca1f1e75e8e
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2024-05-07 23:56:23 +0200

    · Falta veure com agafar els tokens de l'arbre (nodes fulla) i parsejar per l'arrel (primer node).

--- src/main/java/frontEnd/semantics/SemanticAnalyzer.java
+++ src/main/java/frontEnd/semantics/SemanticAnalyzer.java
@@ -1,7 +1,12 @@
 package frontEnd.semantics;
 
 import errorHandlers.SemanticErrorHandler;
-import frontEnd.Tree;
+import frontEnd.lexic.dictionary.Token;
+import frontEnd.sintaxis.Tree;
+import frontEnd.sintaxis.grammar.AbstractSymbol;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SemanticAnalyzer {
     private final SemanticErrorHandler errorHandler;
@@ -27,7 +32,17 @@ public class SemanticAnalyzer {
         errorHandler = semanticErrorHandler;
     }
 
-    public static void sendTree(Tree tree) {
-        //TODO implement this method
+    public static void sendTree(Tree<AbstractSymbol> tree) {
+        // Get a list of terminal symbols (tokens with lexical meaning).
+        List<Token> tokens = new ArrayList<>();
+
+        // Check the first node (root) to see what kind of grammatical operation is done and apply its semantics.
+        switch (tree.getNode().getName()) {
+            case "declaration":
+                break;
+            case "assignation":
+                break;
+            // ...
+        }
     }
 }
