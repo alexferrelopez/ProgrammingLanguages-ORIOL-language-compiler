163cc2e106928ef1a700b542fb6f1ef1e98771a0
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2024-05-15 19:55:18 +0200

    · Funcions d'abans i després de cridar una funció (inicialització de l'stack i frame pointer).

--- src/main/java/backEnd/targetCode/TACToMIPSConverter.java
+++ src/main/java/backEnd/targetCode/TACToMIPSConverter.java
@@ -14,13 +14,20 @@ import java.util.List;
 public class TACToMIPSConverter implements TargetCodeGeneratorInterface {
 	private static final String LINE_SEPARATOR = System.lineSeparator();
 	private static final String LINE_INDENTATION = "\t";
+	private static final String COMMENT_WORD = "#";
+
 	private static final String FRAME_POINTER = "$fp";
 	private static final String STACK_POINTER = "$sp";
 	private static final String RETURN_REGISTER = "$ra";
+	private static final String END_PROGRAM_INSTRUCTION = "syscall";
+	private static final String FUNCTION_RESULT_REGISTER = "$v0";
+
 	private static final String TARGET_FILE = "target/farm.asm";
 	private BufferedWriter targetCode;
 	private final SymbolTableInterface symbolTable;
+
 	private final static String MAIN_FUNCTION = "ranch";
+	private boolean isMainFunction = false;
 
 	public TACToMIPSConverter(SymbolTableInterface symbolTable) {
 		this.symbolTable = symbolTable;
@@ -117,8 +124,13 @@ public class TACToMIPSConverter implements TargetCodeGeneratorInterface {
 		}
 	}
 
+	private String writeComment(String comment) {
+		return (COMMENT_WORD + " " + comment);
+	}
+
 	private String funcDeclaration(String functionLabel) {
-		String text = (functionLabel + ":") + LINE_SEPARATOR;
+		String text = writeComment("Start of function " + functionLabel) + LINE_SEPARATOR +
+				(functionLabel + ":") + LINE_SEPARATOR;
 		/*
 			addi $sp, $sp, -8   # Allocate stack frame
 			sw $ra, 4($sp)      # Save return address
@@ -126,11 +138,11 @@ public class TACToMIPSConverter implements TargetCodeGeneratorInterface {
 			move $fp, $sp       # Set frame pointer
 		 */
 		if (functionLabel.equals(MAIN_FUNCTION)) {
-
+			isMainFunction = true;
 		}
 
 		// Save stack
-		text += LINE_INDENTATION +
+		text += LINE_INDENTATION + writeComment("Save stack, return and frame pointer (from previous call).") + LINE_SEPARATOR + LINE_INDENTATION +
 				("addi " + STACK_POINTER + ", " + STACK_POINTER + ", -8") + LINE_SEPARATOR + LINE_INDENTATION +
 				("sw " + RETURN_REGISTER + ", 4(" + STACK_POINTER + ")") + LINE_SEPARATOR + LINE_INDENTATION +
 				("sw " + FRAME_POINTER + ", 0(" + STACK_POINTER + ")") + LINE_SEPARATOR + LINE_INDENTATION +
@@ -148,7 +160,8 @@ public class TACToMIPSConverter implements TargetCodeGeneratorInterface {
 		else {
 			return functionLabel + ":" + LINE_SEPARATOR;
 		}*/
-		return LINE_INDENTATION + "sub " + STACK_POINTER + ", " + STACK_POINTER + ", " + functionSize + LINE_SEPARATOR;	// sub $sp, $sp, size;
+		return LINE_INDENTATION + "sub " + STACK_POINTER + ", " + STACK_POINTER + ", -" + functionSize + " " +
+				writeComment("Allocate function's memory (in Bytes)") + LINE_SEPARATOR;
 	}
 
 	private String endFunction() {
@@ -159,11 +172,23 @@ public class TACToMIPSConverter implements TargetCodeGeneratorInterface {
 			addi $sp, $sp, 8    # Deallocate stack frame
 			jr $ra              # Return from function
 		 */
-		return	LINE_INDENTATION +
+
+		String text = LINE_INDENTATION + writeComment("End of function - Restore stack, return and frame pointer") + LINE_SEPARATOR + LINE_INDENTATION +
 				("move " + STACK_POINTER + ", " + FRAME_POINTER) + LINE_SEPARATOR + LINE_INDENTATION +
 				("lw " + RETURN_REGISTER + ", 4(" + STACK_POINTER + ")") + LINE_SEPARATOR + LINE_INDENTATION +
 				("lw " + FRAME_POINTER + ", 0(" + STACK_POINTER + ")") + LINE_SEPARATOR + LINE_INDENTATION +
-				("addi " + STACK_POINTER + ", " + STACK_POINTER + ", 8") + LINE_SEPARATOR + LINE_INDENTATION +
-				("jr " + RETURN_REGISTER) + LINE_SEPARATOR;
+				("addi " + STACK_POINTER + ", " + STACK_POINTER + ", 8") + LINE_SEPARATOR + LINE_INDENTATION;
+
+		// End the program if it's the main or add the return value if it's another function.
+		if (isMainFunction) {
+			text += writeComment("End of the main") + LINE_SEPARATOR + LINE_INDENTATION +
+					("li " + FUNCTION_RESULT_REGISTER + ", 10") + LINE_SEPARATOR + LINE_INDENTATION +
+					(END_PROGRAM_INSTRUCTION) + LINE_SEPARATOR;
+		}
+		else {
+			text += ("jr " + RETURN_REGISTER) + LINE_SEPARATOR;
+		}
+
+		return text;
 	}
 }
